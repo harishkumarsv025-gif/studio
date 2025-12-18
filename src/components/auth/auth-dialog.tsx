914ae57cf43e56d430c3a4bef63a9045cfb8e51d
Harish Kumar S V <harishkumarsv025@gmail.com>
2025-12-18 16:18:34 +0000

    again check the errors

--- src/components/auth/auth-dialog.tsx
+++ src/components/auth/auth-dialog.tsx
@@ -10,7 +10,7 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
-import { signInWithGoogle, useUser, getUserProfile, createUserProfile } from '@/firebase';
+import { signInWithGoogle, getUserProfile } from '@/firebase';
 import { CompleteProfileDialog } from './complete-profile-dialog';
 import { User } from 'firebase/auth';
 
